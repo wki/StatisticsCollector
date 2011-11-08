437feb9369bd9b57d63c98cb00d79e34ad9f89e2
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2011-11-08 22:44:18 +0100

    updated deploy procedure

--- script/statisticscollector_rsync.sh
+++ script/statisticscollector_rsync.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 appdir=`dirname $0`/..
-dstdir=/var/www/StatisticsCollector
+dstdir=/home/sites/StatisticsCollector
 cd $appdir
 
 rsync -vcr  --delete \
+  --exclude perl5lib \
   --exclude blib --exclude inc --exclude '.git*' \
   --exclude 'META.*' --exclude 'MYMETA.*' \
   --exclude Makefile \
   --exclude script/dbicdh \
   --exclude Changes --exclude 'README.*' --exclude 'INFO.*' \
-  . root@myloc:$dstdir/ $*
+  . sites@myloc:$dstdir/ $*
 
-ssh root@myloc chown -R www-data:www-data $dstdir
+# ssh root@myloc chown -R www-data:www-data $dstdir
