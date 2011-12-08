d49ad3c2f57f9c303462ee2602ffa5776719d29a
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2011-12-08 21:31:59 +0100

    modified udp server and client

--- script/send_measure.pl
+++ script/send_measure.pl
@@ -2,13 +2,32 @@
 use strict;
 use warnings;
 use IO::Socket::INET;
+use Getopt::Std;
+
+my %opts;
+getopts('hs:p:', \%opts);
+usage() if $opts{h};
+my $server = $opts{s} || '127.0.0.1';
+my $port   = $opts{p} || 8080;
 
 die 'nothing to send' if (!@ARGV);
 
 my $sock = IO::Socket::INET->new(
-    PeerAddr => '127.0.0.1', 
-    PeerPort => 8080,
+    PeerAddr => $server, 
+    PeerPort => $port,
     Proto    => 'udp',
     Timeout  => 1) or die "could not connect: $@";
 
 print $sock join(' ', @ARGV);
+
+
+sub usage {
+    print <<EOF;
+send_measure [options] name/of/sensor [measure]
+   -h       this help
+   -s addr  server (default: 127.0.0.1)
+   -p port  port (default: 8080)
+   
+EOF
+    exit;
+}
--- script/statisticscollector_udp_server.pl
+++ script/statisticscollector_udp_server.pl
@@ -2,50 +2,55 @@
 use strict;
 use warnings;
 use feature ':5.10';
+use FindBin;
+use lib "$FindBin::Bin/../lib";
 use Getopt::Std;
 use IO::Socket::INET;
 use Try::Tiny;
-use FindBin;
-use lib "$FindBin::Bin/../lib";
+use Daemon::Daemonize ':all';
 use StatisticsCollector::Schema;
 
 my %opts;
-getopts('vp:d:h', \%opts);
+getopts('hvp:d:U:P:', \%opts);
 usage() if $opts{h};
-my $verbose = $opts{v} || 0;
-my $port    = $opts{p} || 8080;
-my $dsn     = $opts{d} || 'dbi:Pg:dbname=statistics';
+my $verbose  = $opts{v} || 0;
+my $port     = $opts{p} || 8080;
+my $dsn      = $opts{d} || 'dbi:Pg:dbname=statistics';
+my $user     = $opts{U} || 'statistics';
+my $password = $opts{P} || 'numbers';
 
+daemonize( run => \&process_loop );
+exit;
 
-my $schema = StatisticsCollector::Schema->connect($dsn, 'postgres', '');
-
-
-my $response = IO::Socket::INET->new(
-    Proto     => 'udp',
-    LocalPort => $port,
-    Timeout   => 10)
-or die "Can't make UDP server: $@";
-
-
-say 'UDP Server ready.' if $verbose;
-my ($datagram,$flags);
-while ($response->recv($datagram, 500, $flags)) {
-    say "Got message from ${\$response->peerhost}: $datagram" if $verbose;
+sub process_loop {
+    my $schema = StatisticsCollector::Schema->connect($dsn, $user, $password);
     
-    my ($sensor_name, $value) = split(/\s+/, $datagram);
-    if ($sensor_name !~ m{\A [^/]+ / [^/]+ / [^/]+ \z}xms) {
-        warn "Ignoring sensor '$sensor_name' -- not matching required namespace";
-        next;
+    my $response = IO::Socket::INET->new(
+        Proto     => 'udp',
+        LocalPort => $port,
+        Timeout   => 10)
+    or die "Can't make UDP server: $@";
+    
+    say 'UDP Server ready.' if $verbose;
+    my ($datagram,$flags);
+    while ($response->recv($datagram, 500, $flags)) {
+        say "Got message from ${\$response->peerhost}: $datagram" if $verbose;
+        
+        my ($sensor_name, $value) = split(/\s+/, $datagram);
+        if ($sensor_name !~ m{\A [^/]+ / [^/]+ / [^/]+ \z}xms) {
+            warn "Ignoring sensor '$sensor_name' -- not matching required namespace";
+            next;
+        }
+        $value //= 0;
+        
+        try {
+            $schema->resultset('Sensor')
+                   ->find_or_create( { name => $sensor_name } )
+                   ->add_measure( $value );
+        } catch {
+            warn "Error adding a measure, sensor '$sensor_name' ($_)";
+        };
     }
-    $value //= 0;
-    
-    try {
-        $schema->resultset('Sensor')
-               ->find_or_create( { name => $sensor_name } )
-               ->add_measure( $value );
-    } catch {
-        warn "Error adding a measure, sensor '$sensor_name' ($_)";
-    };
 }
 
 
