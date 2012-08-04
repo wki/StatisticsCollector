1b401a1428f1a4bcd297889f2e7b828e99481b1a
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2012-08-04 21:25:30 +0200

    config/deploy_live.pl

--- config/provision_live.pl
+++ config/provision_live.pl
@@ -3,6 +3,9 @@ use Provision::DSL;
 
 # test with:
 # PERL5LIB=/Users/wolfgang/proj/Provision-DSL/lib /Users/wolfgang/proj/Provision-DSL/bin/provision.pl -c config/deploy_live.pl -n -v
+#
+# when Provision::DSL is installed:
+# provision.pl -c config/deploy_live.pl -n -v
 
 my $APP_NAME        = 'StatiscticsCollector';
 my $DOMAIN          = 'statistics.kinkeldei-net.de',
@@ -17,35 +20,35 @@ my $STATIC_DIR      = "$APP_DIR/$STATIC";
 my $SCRIPT_DIR      = "$APP_DIR/$SCRIPT";
 
 my $CONFIG_DIR      = "$APP_DIR/$CONFIG";
-my $MAIN_CONFIG     = "$CONFIG_DIR/\L$APP_NAME/Q.pl";
+my $MAIN_CONFIG     = "$CONFIG_DIR/\L$APP_NAME\E.pl";
 my $CONFIG_SUFFIX   = 'live';
 
 my $PERL_LIB        = 'local';
 my $PERL5LIB_DIR    = "$APP_DIR/$PERL_LIB/lib/perl5";
 
+my $EXT_PORT        = 81;
+my $INT_PORT        = 5000;
+
 Package 'build-essential';
 Package 'nginx';
 Package 'postgresql-9.1';
 Package 'postgresql-client-9.1';
-Package 'postgresql-server-dev-9.1';
+# Package 'postgresql-server-dev-9.1';
 
 Perlbrew {
+    wanted        => '5.16.0',
     install_cpanm => 1,
-    # wanted  => '5.14.2',
-    wanted  => '5.16.0',
+    tell          => 'perl_updated',
 };
 
 Dir $WEB_DIR => {
-    user => 'root',
+    user       => 'root',
     permission => '0755',
 };
 
 Dir $SITE_DIR => {
-    mkdir   => [
-        'logs', 
-        'pid',
-    ],
-    tell   => 'source_changed',
+    mkdir => ['logs', 'pid'],
+    tell  => 'source_changed',
 };
 
 Dir $APP_DIR => {
@@ -63,7 +66,7 @@ Dir $APP_DIR => {
 ### FIXME: what can we do to ensure all modules are there?
 Execute install_cpan_modules => {
     default_state => 'outdated',     ### bad fake.
-    listen => 'source_changed',
+    listen => ['source_changed', 'perl_updated'],
     path   => Perlbrew->perl,
     cwd    => $APP_DIR,
     args   => [
@@ -80,17 +83,18 @@ foreach my $cache_entry (qw(css/site.css js/site.js)) {
     Execute "cache_$cache_entry" => {
         path => Perlbrew->perl,
         args => [
-            "$SCRIPT_DIR/\L$APP_NAME\Q_test.pl",
+            "$SCRIPT_DIR/\L$APP_NAME\E_test.pl",
             "/$cache_entry",
         ],
         env => {
             PERL5LIBS                           => $PERL5LIB_DIR,
-            "\U$APP_NAME\Q_CONFIG"              => $CONFIG_DIR,
-            "\U$APP_NAME\Q_CONFIG_LOCAL_SUFFIX" => $CONFIG_SUFFIX,
+            "\U$APP_NAME\E_CONFIG"              => $CONFIG_DIR,
+            "\U$APP_NAME\E_CONFIG_LOCAL_SUFFIX" => $CONFIG_SUFFIX,
         },
     };
     
     File "$STATIC_DIR/_$cache_entry" => {
+        ### FIXME: need a failsave condition (mtime static/css/* > this)
         listen  => 'source_changed',
         content => Execute("cache_$cache_entry"),
     };
@@ -101,20 +105,39 @@ foreach my $cache_entry (qw(css/site.css js/site.js)) {
 #     path => '/path/to/website/MyApp/script/db_migration.pl',
 # };
 
-Service "\L$APP_NAME\Q_plack" => {
-    listen  => 'source_changed',
-    content => Resource('service/plack'),
+Service "\L$APP_NAME\E_plack" => {
+    listen  => ['source_changed', 'perl_updated'],
+    content => Template('service/starman', {
+                vars => {
+                    name     => "\L$APP_NAME\E_starman",
+                    app_dir  => $APP_DIR,
+                    starman  => Perlbrew->perl->dir->file('starman'),
+                    port     => $INT_PORT,
+                    pid      => "$SITE_DIR/pid/\L$APP_NAME\E.pid",
+                    psgi     => "$APP_DIR/\L$APP_NAME\E.psgi",
+                    user     => 'vagrant',
+                    group    => 'vagrant',
+                },
+            }),
 };
 
-File "/etc/nginx/sites-enabled/\L$APP_NAME\Q" => {
-    content    => Resource('service/nginx_site'),
+File "/etc/nginx/sites-enabled/\L$APP_NAME\E" => {
+    content    => Template('service/nginx_site', {
+                    vars => {
+                        name     => "\L$APP_NAME\E",
+                        ext_port => $EXT_PORT,
+                        int_port => $INT_PORT,
+                        domain   => $DOMAIN,
+                        app_dir  => $APP_DIR,
+                    },
+                }),
     user       => 'root',
     permission => '0777',
     tell       => 'nginx_config_changed',
 };
 
 Service nginx => {
-    listen => 'nginx_config_changed',
+    listen  => ['source_changed', 'perl_updated', 'nginx_config_changed'],
 };
 
 Done;
